643b5564991017b5c20c47ec857c653dce4bd6a4
jamwitk <hushdilcss@gmail.com>
2020-11-28 22:55:05 +0300

    commiting scene and first code

---
+++ Assets/Scripts/GetDataFromAPI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GetDataFromAPI : MonoBehaviour
+{
+    [System.NonSerialized]
+    public string GetData ="";
+    
+    
+}
+
+[SerializeField]
+public class Weather
+{
+    public static Weather CreateFromJson(string jsonString)
+    {
+        return JsonUtility.FromJson<Weather>(jsonString);
+    }
+}
---
+++ Assets/Scripts/openWeatherInspector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class openWeatherInspector : EditorWindow
+{
+    string _url = "";
+    public string _api = "";
+    public string _city = "";
+    public Texture icon;
+    GetDataFromAPI giveData;
+
+    [MenuItem("Tools/Open Window")]
+    public static void ShowWindow(){
+        GetWindow(typeof(openWeatherInspector));
+    }
+    
+    private void OnGUI()
+    {
+
+        GUILayout.Label("Connect To The OpenWeither", EditorStyles.boldLabel);
+        // Constrain all drawing to be within a 800x600 pixel area centered on the screen.
+        giveData = GameObject.Find("GetDataFromApi").GetComponent<GetDataFromAPI>(); // with giveData this will give url to GetData string in GetDataFromApi.cs
+
+        _api = EditorGUILayout.TextField("Api Key", _api);
+        _city= EditorGUILayout.TextField("City Name ", _city);
+
+        if(GUILayout.Button("Add key"))
+        {
+            _url = "api.openweathermap.org/data/2.5/weather?q=" + CapitalizeString(_city) + "&appid=" + _api;
+            giveData.GetData = _url;
+            Debug.Log(giveData.GetData);
+        }
+       
+       
+    }
+
+    public string CapitalizeString(string capitalize)
+    {
+        
+        string str = char.ToUpper(capitalize[0]) + capitalize.Substring(1);
+        return str;
+    }
+
+}
